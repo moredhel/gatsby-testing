8468582da176ee803e55047a2d39d8829529c0af
Hamish Hutchings <github@aoeu.me>
2020-01-08 20:18:14 +0000

    test out grabbing query params

--- lambdas/hello.go
+++ lambdas/hello.go
@@ -1,14 +1,26 @@
 package main
 
 import (
+	"fmt"
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
 )
 
 func handler(request events.APIGatewayProxyRequest) (*events.APIGatewayProxyResponse, error) {
+	lc, ok := lambdacontext.FromContext(ctx)
+	if !ok {
+		return &events.APIGatewayProxyResponse{
+			StatusCode: 503,
+			Body:       "Something went wrong :(",
+		}, nil
+	}
+
+	cc := lc.ClientContext
+
+	fmt.Printf("%+v\n", request.QueryStringParameters)
 	return &events.APIGatewayProxyResponse{
 		StatusCode: 200,
-		Body:       "Hello, World",
+		Body:       "Hello, " + cc.Client.AppTitle,
 	}, nil
 }
 
