95b03fa8146713e1023e16daf616f22e7622b9d7
Hamish Hutchings <github@aoeu.me>
2020-01-08 20:25:55 +0000

    test out grabbing query params

--- lambdas/hello.go
+++ lambdas/hello.go
@@ -1,16 +1,35 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
 )
 
+type Resp struct {
+	Queries map[string]string
+	Body string
+}
+
 func handler(request events.APIGatewayProxyRequest) (*events.APIGatewayProxyResponse, error) {
 	fmt.Printf("%+v\n", request.QueryStringParameters)
+
+	resp := Resp{
+		Queries: request.QueryStringParameters,
+		Body: request.Body,
+	}
+
+	jsonString, err := json.Marshal(resp)
+	if err != nil {
+		return &events.APIGatewayProxyResponse{
+			StatusCode: 500,
+			Body:       fmt.Sprintf("Could not Parse json\n"),
+		}, err
+	}
 	return &events.APIGatewayProxyResponse{
 		StatusCode: 200,
-		Body:       fmt.Sprintf("%+v", request.Body),
+		Body:       fmt.Sprintf("%s\n", jsonString),
 	}, nil
 }
 
